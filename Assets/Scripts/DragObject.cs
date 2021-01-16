6149fd1421805b5dd5f9de39d597b7958996eea6
ReportHarambe <58485947+ReportHarambe@users.noreply.github.com>
2021-01-16 03:10:37 +0100

    Initial commit, ajout du projet
    Ajout du projet en espérant que vous puissiez pull

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+    public bool clicked;
+    public int value;
+    
+
+    private float mZCoord;
+    public bool dropped;
+    
+
+    void OnMouseDown()
+    {
+        if (dropped == false)
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+        }
+     
+
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        if (dropped == false)
+        {
+            transform.position = new Vector3(GetMouseWorldPos().x + mOffset.x, transform.position.y, GetMouseWorldPos().z + mOffset.z);
+        }
+
+
+    }
+    void OnMouseUp()
+    {
+        dropped = true;
+      
+        if (clicked == false)
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.isKinematic = false;
+            clicked = true;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/EarthScript.cs
@@ -0,0 +1,29 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthScript : MonoBehaviour
+{
+    public float thrust = 9.81f;
+    public Rigidbody rb;
+
+    public void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust);
+    }
+}
