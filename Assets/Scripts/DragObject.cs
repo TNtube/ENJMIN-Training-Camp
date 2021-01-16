74ce8ee496f50360ebff4e3f69ccbefcd29cb0d8
ReportHarambe <58485947+ReportHarambe@users.noreply.github.com>
2021-01-16 03:55:16 +0100

    Ajout du GameMananger, suppression de collisions lors de la selection
    Ajout du GameManager
    Modification de DragObject pour utiliser le Rigidbody des prefabs
    Suppression des collisions avant qu'un objet soit laché en mettant IsTrigger des prefabs à true, puis false quand ils sont lachés

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -6,23 +6,9 @@ public class DragObject : MonoBehaviour
 {
     private Vector3 mOffset;
     public bool clicked;
-    public int value;
-    
 
     private float mZCoord;
     public bool dropped;
-    
-
-    void OnMouseDown()
-    {
-        if (dropped == false)
-        {
-            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-            mOffset = gameObject.transform.position - GetMouseWorldPos();
-        }
-     
-
-    }
 
     private Vector3 GetMouseWorldPos()
     {
@@ -40,19 +26,33 @@ public class DragObject : MonoBehaviour
             transform.position = new Vector3(GetMouseWorldPos().x + mOffset.x, transform.position.y, GetMouseWorldPos().z + mOffset.z);
         }
 
+    }
+
+    void OnMouseDown()
+    {
+        if (dropped == false)
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+        }
 
     }
+
     void OnMouseUp()
     {
         dropped = true;
-      
+
         if (clicked == false)
-        {
+        { 
             Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-            rb.useGravity = true;
             rb.isKinematic = false;
+            rb.useGravity = true;
+            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+
             clicked = true;
-            
+
+            GameObject.Find("GameController").GetComponent<GameManager>().OnPlaced();
+
         }
     }
 }
