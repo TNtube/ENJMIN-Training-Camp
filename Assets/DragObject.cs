86f0dd77d349f94cd97711e22f5737a89223fed7
tyoumage <57842806+tyoumage@users.noreply.github.com>
2021-01-16 13:28:18 +0100

    Protorype0.1
    Version avec première scène, avec terre et 3 prefabs.

---
+++ Assets/DragObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+    public bool clicked;
+    public int value;
+    public Rigidbody gameObjectsRigidBody;
+
+    private float mZCoord;
+    public bool dropped;
+
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        if (dropped == false)
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+        }
+
+
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    // Update is called once per frame
+    void OnMouseDrag()
+    {
+        if (dropped == false)
+        {
+            transform.position = new Vector3(GetMouseWorldPos().x + mOffset.x, transform.position.y, GetMouseWorldPos().z + mOffset.z);
+        }
+
+
+    }
+    void OnMouseUp()
+    {
+        dropped = true;
+
+        if (clicked == false)
+        {
+            gameObjectsRigidBody = gameObject.AddComponent<Rigidbody>();
+
+            clicked = true;
+            switch (value)
+            {
+                case 0:
+                    gameObjectsRigidBody.mass = 1;
+                    break;
+
+                case 1:
+                    gameObjectsRigidBody.mass = 3;
+                    break;
+
+                case 2:
+                    gameObjectsRigidBody.mass = 7;
+                    break;
+            }
+
+            GameObject.Find("Main Camera").GetComponent<GameController>().OnPlaced();
+
+        }
+    }
+}
+
---
+++ Assets/cameraController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    private float moveSpeed = 0.5f;
+    private float scrollSpeed = 10f;
+
+    float horizontalInput;
+    float verticalInput;
+    float wheelInput;
+
+    public float speedH = 5.0f;
+    public float speedV = 5.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+    void Update()
+    {
+        Vector3 rotation = transform.eulerAngles;
+
+
+
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.transform.position += Camera.main.transform.forward * ScrollWheelChange * 3;
+    }
+
+    void FixedUpdate()
+    {
+
+
+        if (Input.GetMouseButton(2))
+        {
+
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                Camera.main.transform.position += moveSpeed / 2 * -Camera.main.transform.right;
+            }
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                Camera.main.transform.position += moveSpeed / 2 * Camera.main.transform.right;
+            }
+
+            if (Input.GetAxis("Mouse Y") > 0)
+            {
+                Camera.main.transform.position += moveSpeed / 2 * -Camera.main.transform.up;
+            }
+            if (Input.GetAxis("Mouse Y") < 0)
+            {
+                Camera.main.transform.position += moveSpeed / 2 * Camera.main.transform.up;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            transform.position += scrollSpeed * new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        }
+
+
+
+        if (Input.GetMouseButton(1))
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+}
