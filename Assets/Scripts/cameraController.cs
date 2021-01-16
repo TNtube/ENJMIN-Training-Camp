c8767111e66ecec14657b0f68c3857fb3701ea9f
ReportHarambe <58485947+ReportHarambe@users.noreply.github.com>
2021-01-16 14:29:40 +0100

    Arbre attaché à la plateforme, CameraController
    Ajout de CameraController dans script,
    attachement de l'arbre à la plateforme

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     private float moveSpeed = 0.5f;
     private float scrollSpeed = 10f;
